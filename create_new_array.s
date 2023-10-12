5345263cfabf911ed676858d97e836ceb0e4f331
Alex <yevsyukovaleks@gmail.com>
2023-10-12 03:42:37 +0300

    IHW1 v1

---
+++ create_new_array.s
@@ -0,0 +1,37 @@
+.global create_new_array
+
+create_new_array:
+	la		t0 array_A		# Указатель элемента массива A
+	la		t1 array_B		# Указатель элемента массива B
+	lw		t3 n			# Число элементов массива
+	lw		t4 min_number	# Адрес min_number в t4
+	sw		t4 (t1)			# Загрузка first_number в массив B
+	lw		t5 first_number	# Адрес first_number в t5
+	addi	t0 t0 4			# Увеличим адрес на размер слова в байтах в A
+	addi	t1 t1 4			# Увеличим адрес на размер слова в байтах в B
+	addi	t3 t3 -1		# Уменьшим количество оставшихся элементов на 1
+	
+loop:	
+	lw		t6 (t0)			# Вытаскиваем элемент массива и кладем его в t5
+	beq 	t6 t4 swap		# Если t6 == min_number, меняем элементы местами
+back3:	
+	sw		t6 (t1)			# Загрузка элемента в массив B
+	addi	t0 t0 4			# Увеличим адрес на размер слова в байтах
+	addi	t1 t1 4			# Увеличим адрес на размер слова в байтах в B
+	addi	t3 t3 -1		# Уменьшим количество оставшихся элементов на 1
+	bnez	t3 loop			# Если осталось больше 0
+	clean(t0)				# Очистка t0
+	clean(t1)				# Очистка t1
+	clean(t2)				# Очистка t1
+	clean(t3)				# Очистка t3
+	clean(t4)				# Очистка t4
+	clean(t5)				# Очистка t5
+	clean(t6)				# Очистка t6
+	ret
+
+# Меняем элементы местами
+swap:	
+	bgtz	t2 back3
+	mv		t6 t5
+	addi	t2 t2 1
+	j		back3
---
+++ macrolib.s
@@ -0,0 +1,107 @@
+# Печать содержимого регистра как целого
+.macro print_int(%x)
+        li      a7, 1
+        mv      a0, %x
+        ecall
+.end_macro
+
+# Ввод целого числа с консоли в регистр a0
+.macro read_int_a0
+        li       a7, 5
+        ecall
+.end_macro
+
+# Ввод целого числа с консоли в указанный регистр, исключая регистр a0
+.macro read_int(%x)
+        push     (a0)
+        li       a7, 5
+        ecall
+        mv       %x, a0
+        pop      (a0)
+.end_macro
+
+# Вывод строки 
+.macro print_str(%x)
+   .data
+str:
+        .asciz   %x
+   .text
+        push     (a0)
+        li       a7, 4
+        la       a0, str
+        ecall
+        pop      (a0)
+.end_macro
+
+# Вывод символа
+.macro print_char(%x)
+        li       a7, 11
+        li       a0, %x
+        ecall
+.end_macro
+
+# Новая строка
+.macro newline
+        print_char('\n')
+.end_macro
+
+# Сохранение заданного регистра на стеке
+.macro push(%x)
+        addi    sp, sp, -4
+        sw      %x, (sp)
+.end_macro
+
+# Выталкивание значения с вершины стека в регистр
+.macro pop(%x)
+        lw      %x, (sp)
+        addi    sp, sp, 4
+.end_macro
+
+# Очистка регистра
+.macro clean(%x)
+	li	%x 0
+.end_macro
+
+# Вызов подпрограммы, которая читаем n
+.macro input_n(%x)
+	la	t4 %x			# Адрес n в t4
+	jal     input_n	
+.end_macro
+
+# Вызов подпрограммы, которая читаем вводимый массив
+.macro input_arr(%x, %y)
+	la	t0 %x			# Указатель элемента массива
+ 	lw	t3 %y			# Число элементов массива
+	jal	input_arr	
+.end_macro
+
+# Вызов подпрограммы, которая ищет минимальный элемент массива
+.macro find_min
+        jal     find_min	
+.end_macro
+
+# Вызов подпрограммы, которая создает массив B
+.macro create_new_array
+        jal     create_new_array	
+.end_macro
+
+# Вызов подпрограммы, которая выводит результат
+.macro output(%x, %y)
+	la	t1 %x                   # Указатель элемента массива B
+	lw	t3 %y			# Число элементов массива
+	jal     output
+.end_macro
+
+# Запрос на повторный запуск программы
+.macro repeat
+	print_str("\nIf you want to exit, enter 0. Otherwise, enter another number   ")
+	read_int(t1)		        # Чтение ответа
+	newline
+	bnez t1 main		        # Повторный запуск	
+.end_macro
+
+# Завершение программы
+.macro exit
+	li      a7, 10
+	ecall
+.end_macro
