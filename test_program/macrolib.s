addca788d602fc4947b2e918f34445cf1d29a94c
Alex <yevsyukovaleks@gmail.com>
2023-10-12 21:42:14 +0300

    IHW1 v4

--- test_program/macrolib.s
+++ test_program/macrolib.s
@@ -35,9 +35,9 @@ str:
 
 # Вывод символа
 .macro print_char(%x)
-        li       a7, 11
-        li       a0, %x
-        ecall
+    li       a7, 11
+    li       a0, %x
+    ecall
 .end_macro
 
 # Новая строка
@@ -47,55 +47,55 @@ str:
 
 # Сохранение заданного регистра на стеке
 .macro push(%x)
-        addi    sp, sp, -4
-        sw      %x, (sp)
+    addi    sp, sp, -4
+    sw      %x, (sp)
 .end_macro
 
 # Выталкивание значения с вершины стека в регистр
 .macro pop(%x)
-        lw      %x, (sp)
-        addi    sp, sp, 4
+    lw      %x, (sp)
+    addi    sp, sp, 4
 .end_macro
 
 # Очистка регистра
 .macro clean(%x)
-	li	%x 0
+	li		%x 0
 .end_macro
 
 # Вызов подпрограммы, которая читаем n
 .macro input_n(%x)
-	la	t4 %x			# Адрес n в t4
+	la		t4 %x			# Адрес n в t4
 	jal     input_n	
 .end_macro
 
 # Вызов подпрограммы, которая читаем вводимый массив
 .macro input_arr(%x, %y)
-	la	t0 %x			# Указатель элемента массива
- 	lw	t3 %y			# Число элементов массива
-	jal	input_arr	
+	la		t0 %x			# Указатель элемента массива
+ 	lw		t3 %y			# Число элементов массива
+	jal		input_arr	
 .end_macro
 
 # Вызов подпрограммы, которая ищет минимальный элемент массива
 .macro find_min(%n, %min, %array)
-	la	t0 %array		# Указатель элемента массива
-	lw	t3 %n			# Число элементов массива
-	la	t6 %min		        # Адрес min_number в t4
-	jal	find_min	
+	la		t0 %array		# Указатель элемента массива
+	lw		t3 %n			# Число элементов массива
+	la		t6 %min		        # Адрес min_number в t4
+	jal		find_min	
 .end_macro
 
 # Вызов подпрограммы, которая создает массив B
 .macro create_new_array(%n, %array_A, %array_B, %min)
-	la      t0 %array_A		# Указатель элемента массива A
-	la      t1 %array_B		# Указатель элемента массива B
-	lw      t3 %n			# Число элементов массива
-	lw      t4 %min  	# Адрес min_number в t4
-        jal     create_new_array	
+	la		t0 %array_A		# Указатель элемента массива A
+	la		t1 %array_B		# Указатель элемента массива B
+	lw		t3 %n			# Число элементов массива
+	lw		t4 %min	# Адрес min_number в t4
+	jal		create_new_array	
 .end_macro
 
 # Вызов подпрограммы, которая выводит результат
 .macro output(%x, %y)
-	la	t1 %x                   # Указатель элемента массива B
-	lw	t3 %y			# Число элементов массива
+	la		t1 %x                   # Указатель элемента массива B
+	lw		t3 %y			# Число элементов массива
 	jal     output
 .end_macro
 
@@ -104,7 +104,14 @@ str:
 	print_str("\nIf you want to exit, enter 0. Otherwise, enter another number   ")
 	read_int(t1)		        # Чтение ответа
 	newline
-	bnez t1 main		        # Повторный запуск	
+	bnez 	t1 main		        # Повторный запуск	
+.end_macro
+
+# Добавление элемента в массив
+.macro array_add(%x, %y)
+	li 		a0 %x
+	sw 		a0(%y)
+	addi 	%y %y 4
 .end_macro
 
 # Завершение программы
---
+++ test_program/main.s
@@ -0,0 +1,108 @@
+.include "macrolib.s"
+.include "input_n.s"
+.include "input_arr.s"
+.include "find_min.s"
+.include "create_new_array.s"
+.include "output.s"
+
+.global main
+
+main:
+.data
+	.align  2						# Выравнивание на границу слова
+	n1:              .word	0		# Число введенных элементов массива
+	array_A1:        .space 64		# Массив А
+	array_B1:        .space 64		# Массив B
+	min_number1:     .word	0		# Минимальное значение
+	first_number:	   .word	0	# Первый элемент массива
+	
+	n2:              .word	0		# Число введенных элементов массива
+	array_A2:        .space 64		# Массив А
+	array_B2:        .space 64		# Массив B
+	min_number2:     .word	0		# Минимальное значение
+	
+	n3:              .word	0		# Число введенных элементов массива
+	array_A3:        .space 64		# Массив А
+	array_B3:        .space 64		# Массив B
+	min_number3:     .word	0		# Минимальное значение
+	
+.text	
+	print_str("Test #1")
+	newline
+	print_str("n = 5")
+	newline
+	# Инициализация n
+	la	t4 n1	
+	li 	a0 5					
+	sw	a0 (t4)
+	print_str("Array A: [3 5 1 2 4]")
+	newline
+	print_str("Expected result [1 5 3 2 4]")
+	la	t4 array_A1
+	array_add(3, t4)
+	array_add(5, t4)
+	array_add(1, t4)
+	array_add(2, t4)
+	array_add(4, t4)
+	clean(t4)
+	clean(a0)	
+	find_min(n1, min_number1, array_A1)
+	# ������� ������ B
+	create_new_array(n1, array_A1, array_B1, min_number1)			
+	newline
+	output(array_B1, n1)					
+	newline
+	print_str("______________")
+	newline
+	
+	print_str("Test #2")
+	newline
+	print_str("n = 5")
+	newline
+	# Инициализация n
+	la	t4 n2
+	li 	a0 5
+	sw	a0 (t4)
+	print_str("Array A: [2 8 -1 3 -4]")
+	newline
+	print_str("Expected result [-4 8 -1 3 2]")
+	la	t4 array_A2
+	array_add(2, t4)
+	array_add(8, t4)
+	array_add(-1, t4)
+	array_add(3, t4)
+	array_add(-4, t4)
+	clean(t4)
+	clean(a0)	
+	find_min(n2, min_number2, array_A2)
+	create_new_array(n2, array_A2, array_B2, min_number2)
+	newline
+	output(array_B2, n2)					
+	newline
+	print_str("______________")
+	newline
+	
+	print_str("Test #3")
+	newline
+	print_str("n = 3")
+	newline
+	# Инициализация n
+	la	t4 n3
+	li 	a0 3
+	sw	a0 (t4)
+	print_str("Array A: [2 1 1]")
+	newline
+	print_str("Expected result [1 2 1]")
+	la	t4 array_A3
+	array_add(2, t4)
+	array_add(1, t4)
+	array_add(1, t4)
+	clean(t4)
+	clean(a0)	
+	find_min(n3, min_number3, array_A3)
+	create_new_array(n3, array_A3, array_B3, min_number3)				
+	newline
+	output(array_B3, n3)
+	newline
+	
+	exit							# Завершение программы
